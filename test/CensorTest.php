851e1aef92cb8f0aadd2a8ca2648aff2d07952d5
Romain Clair <romain.clair@wildcodeschool.fr>
2021-06-21 11:44:26 +0200

    Improving test suite

--- test/CensorTest.php
+++ test/CensorTest.php
@@ -7,40 +7,39 @@ use wcs\Censor;
 
 class CensorTest extends TestCase
 {
-    public function testEmptySentence()
+    public function testCensorExists()
     {
-        $this->assertSame("", Censor::censor("", ""));
-        $this->assertSame("", Censor::censor("", "Something"));
+        $this->assertTrue(method_exists(Censor::class, "censor"), "The Censor::censor static method does not exists");
     }
 
-    public function testEmptyForbidden()
+    /**
+     * @depends testCensorExists
+     * @dataProvider sentenceProvider
+     */
+    public function testEmptySentence(string $expected, string $toCensor, string $forbiddenWord, string $message)
     {
-        $this->assertSame("Some phrase", Censor::censor("Some phrase", ""));
-        $this->assertSame("Une phrase accentuée", Censor::censor("Une phrase accentuée", ""));
+        $this->assertSame($expected, Censor::censor($toCensor, $forbiddenWord), $message);
     }
 
-    public function testModifiedOnce()
+    public function sentenceProvider(): array
     {
-        $this->assertSame("S*******", Censor::censor("Sentence", "Sentence"));
-        $this->assertSame("V******** is back", Censor::censor("Voldemort is back", "voldemort"));
-    }
-
-    public function testMultipleModification()
-    {
-        $this->assertSame("W*** and W***", Censor::censor("Word and Word", "Word"));
-        $this->assertSame(
-            "En variant le ton, – par exemple, ten** : Agressif : « Moi, monsieur, si j’avais un tel n**, il faudrait sur-le-champ que je me l’amputasse ! »",
-            Censor::censor(
+        return [
+            ["", "", "", "Censoring empty sentences must return empty results"],
+            ["", "", "Something", "Censoring empty sentences must return empty results"],
+            ["Some phrase", "Some phrase", "", "Censoring using a blank word must let sentences unchanged"],
+            ["Une phrase accentuée", "Une phrase accentuée", "", "Censoring using a blank word must let sentences unchanged"],
+            ["S*******", "Sentence", "Sentence", "Censoring Sentence in Sentence must return S*******"],
+            ["V******** is back", "Voldemort is back", "voldemort", "Censoring voldemort in 'Voldemort is back' must return V******** is back"],
+            ["W*** and W***", "Word and Word", "Word", "Censoring Word in 'Word and Word' must return W*** and W***"],
+            [
+                "En variant le ton, – par exemple, ten** : Agressif : « Moi, monsieur, si j’avais un tel n**, il faudrait sur-le-champ que je me l’amputasse ! »",
                 "En variant le ton, – par exemple, tenez : Agressif : « Moi, monsieur, si j’avais un tel nez, il faudrait sur-le-champ que je me l’amputasse ! »",
-                "nez"
-            )
-        );
+                "nez", "Censoring nez in 'En variant le ton, – par exemple, tenez : Agressif : « Moi, monsieur, si j’avais un tel nez, il faudrait sur-le-champ que je me l’amputasse ! »' must return 'En variant le ton, – par exemple, ten** : Agressif : « Moi, monsieur, si j’avais un tel n**, il faudrait sur-le-champ que je me l’amputasse ! »'"
+            ],
+            ["W*** and w***", "Word and word", "Word", "Censoring Word in 'Word and word' must return 'W*** and w***'"],
+            ["W*** and w***", "Word and word", "word", "Censoring word in 'Word and word' must return 'W*** and w***'"],
+            
+           
+        ];
     }
-
-    public function testMultipleModificationCaseInsensitive()
-    {
-        $this->assertSame("W*** and w***", Censor::censor("Word and word", "Word"));
-        $this->assertSame("W*** and w***", Censor::censor("Word and word", "word"));
-    }
-    
 }
