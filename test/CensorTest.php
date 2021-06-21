06ef694a387638cf7802fc8ef0818f1fa7419ff3
Romain Clair <romain.clair@wildcodeschool.fr>
2021-06-21 11:50:22 +0200

    Fix some codestyle issues

--- test/CensorTest.php
+++ test/CensorTest.php
@@ -27,14 +27,25 @@ class CensorTest extends TestCase
             ["", "", "", "Censoring empty sentences must return empty results"],
             ["", "", "Something", "Censoring empty sentences must return empty results"],
             ["Some phrase", "Some phrase", "", "Censoring using a blank word must let sentences unchanged"],
-            ["Une phrase accentuée", "Une phrase accentuée", "", "Censoring using a blank word must let sentences unchanged"],
+            [
+                "Une phrase accentuée",
+                "Une phrase accentuée",
+                "",
+                "Censoring using a blank word must let sentences unchanged"
+            ],
             ["S*******", "Sentence", "Sentence", "Censoring Sentence in Sentence must return S*******"],
-            ["V******** is back", "Voldemort is back", "voldemort", "Censoring voldemort in 'Voldemort is back' must return V******** is back"],
+            [
+                "V******** is back",
+                "Voldemort is back",
+                "voldemort",
+                "Censoring voldemort in 'Voldemort is back' must return V******** is back"
+            ],
             ["W*** and W***", "Word and Word", "Word", "Censoring Word in 'Word and Word' must return W*** and W***"],
             [
                 "En variant le ton, – par exemple, ten** : Agressif : « Moi, monsieur, si j’avais un tel n**, il faudrait sur-le-champ que je me l’amputasse ! »",
                 "En variant le ton, – par exemple, tenez : Agressif : « Moi, monsieur, si j’avais un tel nez, il faudrait sur-le-champ que je me l’amputasse ! »",
-                "nez", "Censoring nez in 'En variant le ton, – par exemple, tenez : Agressif : « Moi, monsieur, si j’avais un tel nez, il faudrait sur-le-champ que je me l’amputasse ! »' must return 'En variant le ton, – par exemple, ten** : Agressif : « Moi, monsieur, si j’avais un tel n**, il faudrait sur-le-champ que je me l’amputasse ! »'"
+                "nez",
+                "Censoring nez in 'En variant le ton, – par exemple, tenez : Agressif : « Moi, monsieur, si j’avais un tel nez, il faudrait sur-le-champ que je me l’amputasse ! »' must return 'En variant le ton, – par exemple, ten** : Agressif : « Moi, monsieur, si j’avais un tel n**, il faudrait sur-le-champ que je me l’amputasse ! »'"
             ],
             ["W*** and w***", "Word and word", "Word", "Censoring Word in 'Word and word' must return 'W*** and w***'"],
             ["W*** and w***", "Word and word", "word", "Censoring word in 'Word and word' must return 'W*** and w***'"],
